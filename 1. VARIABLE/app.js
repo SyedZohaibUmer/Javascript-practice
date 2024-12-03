eb1626f3f777e5f9d63ae88f52063450efb06171
Syed Zohaib Umer <syedzohaibumer@gmail.com>
2024-12-04 00:16:56 +0500

    Initial commit

---
+++ 1. VARIABLE/app.js
@@ -0,0 +1,101 @@
+// // // // // alert("Hello World");
+// // // // // var FullName = "Syed zohaib Umer";
+// // // // // age = 21;
+// // // // // isfolllow = true;
+// // // // // x = null;
+// // // // // console.log(FullName);
+// // // // // console.log(age);
+// // // // // console.log(isfolllow0);
+// // // // // console.log(x);
+
+// // // // var a = 10;
+// // // // console.log(a);
+// // // // a = "Hello World";
+// // // // console.log(a);
+// // // let b = "zohaib";
+// // // b = "4";
+// // // {
+// // //     let  b = "syed"
+// // //     console.log(b);
+// // // }
+// // // console.log(b);
+// // const b = "1947"
+// // b = "1948"
+// // console.log(b);
+// // let a = 10;
+// // let b = 20;
+// // let c = a;
+// // a = b;
+// // b = c;
+// // console.log(a);
+// // console.log(b);
+// // let syed = "1947"
+// // if  (syed < 1947, print = 1948,i++,
+
+// // );
+// // else
+// //     alert =("this code is incorrect");
+
+// // class 04 data types
+// // // nn bb ss u
+// // let a = null;
+// // let b = 345;
+// // let c = true;
+// // let d = BigInt("567") + BigInt("33");  
+// // let e = "zohaib"
+// // let f = symbol("Hey my name is zohaib")
+// // let g = undefined;
+// // console.log(a, b, c, d, e, f, g,)
+
+// class 5 ptactice test
+// Q1.
+// let a = "harry";
+// let b = "650";
+// console.log(a + b );
+
+// // Q2.
+// console.log(typeof (a+b));
+
+// const a = {
+//     name: "zohiab";
+//     class: "12th";
+//     isfollow: "true";
+// }
+// a = "syed";
+
+//1: VARIABLE FOR STRING:
+
+// var name1 = "syed zohaib umer";
+// var name1 = "Thank you1";
+// alert= prompt("Enter your age");
+// console.log(name1);
+//  let fullname1 = "syed zohaib umer";
+//  let fullname = "Thank you for the alert";//
+//  alert(fullname);
+//  console.log(fullname);
+
+//2: VARIABLE FOR NUMBER/ Text:
+
+// var num1 = 50;
+// var num2 = 48;
+// var num3 = num1 + num2 + 2
+// console.log(num3);
+
+// var originalNum = 23;
+// originalNum = originalNum * 2;
+//   var newNum = originalNum + 7;
+//  console.log(originalNum);
+//   alert(newNum);
+
+// //3: VARIABLE MATHS EXPRESSION/FAMILIAR OPERATORS
+//  var addnum = 2 + 2;
+//  var addnum1 = 2 * 2;
+//  var addnum2 = 4 / 2;
+//  console.log(addnum);
+//  console.log(addnum1);
+//  console.log(addnum2);
+
+//4: VARIABLE MATHS EXPRESSION/UNFAMILIAR OPERATORS
+ var num = 2;
+ var newNum = num++;
+ console.log(newNum);
