f1fcc3a22a4f30fdd5f08b91fb21badf87500514
Syed Zohaib Umer <syedzohaibumer@gmail.com>
2024-12-24 03:38:57 +0500

    Arrays and its methods

--- 3. ARRAYS/app.js
+++ 3. ARRAYS/app.js
@@ -1,23 +1,62 @@
 let arr = [1,2,3,4,5,6]
-arr [2] = 9
+
+arr [2] = 
+// 1. toString()
 arr.toString();
+
+// 2. join()
+console.log(arr.join(" and "))
+
+// 3. pop()
 arr.pop();
+
+// 4. push()
 arr.push(10);
+
+// 5. Shift()
 arr.shift()
+
+// 6. Unshift()
 arr.unshift("zohaib");
+
+// 7. Delete []
 delete arr[5]
 
 
-console.log(arr.join(" and "))
 console.log(arr); 
 console.log(arr.length)
 
-let a1 = [1,2,3,4];
-let a2 = [5,6,7,8];
-let a3 = [9,10,11,12];
+// 8. Concat()
+let a = [1,2,3,4];
+let b = [5,6,7,8];
+let c = [9,10,11,12];
 
-let result = a1.concat(a2,a3);
-console.log(result)
+let result = a.concat(b,c);
+console.log(result);
+
+// 9. shot()
+let sort = [2,5,3,6,4,5,1]
+sort.sort();
+    console.log(sort);
+    // EX:2
+    let sort1 = ['g','f','b','e','c','a','d']
+    sort1.sort();
+    console.log(sort1);
+
+//10. Splice
+let  abc1 = [1,2,3,4,5,6,7,8];
+abc1.splice(1,2);
+console.log(abc1);
+let  abc2 = [1,2,3,4,5,6,7,8];
+abc2.splice(1,2, 33,'Hello');
+console.log(abc2);
+
+// 11. Slice 
+let array = [1,2,3,4,5,6,7,8];
+let slicearray = array.slice(1,5);
+console.log(array); 
+console.log(slicearray); 
+
 
 // var city0 = "Atlanta";
 // var city1 = "Baltimore";
