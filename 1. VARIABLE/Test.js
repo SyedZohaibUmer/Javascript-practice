1a61ba28462ea063320125af36e1fddfc97d29cb
Syed Zohaib Umer <syedzohaibumer@gmail.com>
2024-12-29 06:36:03 +0500

    Practice Test

---
+++ 1. VARIABLE/Test.js
@@ -0,0 +1,17 @@
+// 1.Create a variable of type string and try to add a number to it.
+var abc = "zohaib"
+var abc1 = 24
+
+let result = abc + abc1;
+console.log(result);
+
+// 2. Use typeof operator to find the data type of the string in the last question.
+console.log(typeof result);
+
+String
+// 3. Create a const object in javascript. can you change it to hold a number later?
+ const obj1 = {name:"zohaib"}
+ obj1.name = "zohaib umer";
+console.log(obj1)
+
+
