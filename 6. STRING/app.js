c98382a9eaaecdf65824835defd255855bc7bb99
Syed Zohaib Umer <syedzohaibumer@gmail.com>
2025-02-27 03:30:36 +0500

    Strings

---
+++ 6. STRING/app.js
@@ -0,0 +1,22 @@
+let name = "zohaib";
+console.log(name);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]);
+console.log(name.length);
+
+//Template Strings
+
+let myname ="zoahib";
+let friendname ="abdullah"
+
+console.log("my name is "+ myname + " and my friend name is "+friendname);
+console.log(`my name is ${myname} and my friend name is ${friendname}`);
+
+
+console.log(myname.toUpperCase());
+console.log(myname.toLowerCase());
+console.log(myname.slice(2,4));
+console.log(myname.replace("zo","45"));
